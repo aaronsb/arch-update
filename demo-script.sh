03daa34424d7bcacd04e62620b28236cf2f34839
Aaron Bockelie <aaronsb@gmail.com>
2025-03-11 13:37:22 -0500

    Demo script for docs

--- demo-script.sh
+++ demo-script.sh
@@ -14,24 +14,55 @@ clear
 print_header "Arch Linux Update Tool Demo"
 sleep 2
 
+# Deploy the update tool
+clear
+print_header "Deploy the update tool from the cloned repository"
+sleep 2
+echo
+./deploy.sh
+sleep 3
+
 # Show help
+clear
+print_header "Basic usage"
+sleep 2
 type_cmd "./update.sh --help"
 sleep 1
 echo
 ./update.sh --help
 sleep 3
 
+
 # Show dry-run
+clear
+print_header "Dry-run mode"
+sleep 2
 echo
 type_cmd "./update.sh --dry-run"
 sleep 1
 echo
 ./update.sh --dry-run
 sleep 3
+sudo -k
 
 # Show actual update
+clear
+print_header "Actual update mode"
+sleep 2
 echo
-type_cmd "./update.sh --run --confirm"
+type_cmd "./update.sh --run"
 sleep 1
 echo
-./update.sh --run --confirm
+./update.sh --run
+sleep 3
+sudo -k
+
+# Create a new module from a template.
+clear
+print_header "Create a new module from a template with interactive dialogs"
+sleep 2
+echo
+type_cmd "./update.sh --create-module"
+sleep 1
+echo
+./update.sh --run
